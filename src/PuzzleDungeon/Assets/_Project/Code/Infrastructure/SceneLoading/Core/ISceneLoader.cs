a67d4ace4e9ee90382bad5bc6c57d46ba64c443f
d4nilevi4 <d4nia14@gmail.com>
2026-01-09 22:52:29 +0300

    Add SceneLoading service

---
+++ src/PuzzleDungeon/Assets/_Project/Code/Infrastructure/SceneLoading/Core/ISceneLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace PuzzleDungeon.Infrastructure
+{
+    public interface ISceneLoader
+    {
+        [MustUseReturnValue]
+        UniTask LoadSceneAsync(string sceneName, SceneLoadMode loadMode, CancellationToken cancellationToken);
+    }
+}
---
+++ src/PuzzleDungeon/Assets/_Project/Code/Infrastructure/SceneLoading/Core/SceneLoadMode.cs
@@ -0,0 +1,8 @@
+namespace PuzzleDungeon.Infrastructure;
+
+public enum SceneLoadMode
+{
+    None = 0,
+    Single = 1,
+    Additive = 2
+}
