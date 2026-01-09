2320dcf6ab60b13a49e6c81aa52d438da375f97a
d4nilevi4 <d4nia14@gmail.com>
2026-01-09 22:54:01 +0300

    define project entry point

---
+++ src/PuzzleDungeon/Assets/_Project/Code/Infrastructure/EntryPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using VContainer;
+using VContainer.Unity;
+
+namespace PuzzleDungeon.Infrastructure
+{
+    public class EntryPoint : IAsyncStartable
+    {
+        private IGameStateMachine _gameStateMachine;
+
+        [Inject]
+        private void Construct(IGameStateMachine gameStateMachine)
+        {
+            _gameStateMachine = gameStateMachine;
+        }
+
+        public UniTask StartAsync(CancellationToken cancellation)
+        {
+            return _gameStateMachine.Enter<BootstrapState>(cancellation);
+        }
+    }
+}
