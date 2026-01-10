48a76158e5959ed223f7b735a4c93124c59918c9
d4nilevi4 <d4nia14@gmail.com>
2026-01-10 13:33:38 +0300

    add entity view contract

---
+++ src/PuzzleDungeon/Assets/_Project/Code/Infrastructure/View/IEntityView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace PuzzleDungeon.Infrastructure;
+
+public interface IEntityView
+{
+    Vector3 Position { get; set; }
+    Quaternion Rotation { get; set; }
+    Vector3 Scale { get; set; }
+    
+    bool IsVisible { get; set; }
+    
+    void Destroy();
+}
