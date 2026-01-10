c60f41c08719a1480ba8912cc096103a05fbedc9
d4nilevi4 <d4nia14@gmail.com>
2026-01-10 19:56:54 +0300

    Add VContainer domain

---
+++ src/PuzzleDungeon/Assets/_Project/Code/VContainer/GameStateMachine/PooledStateFactory.cs
@@ -0,0 +1,31 @@
+using PuzzleDungeon.Core.GameStateMachine;
+using PuzzleDungeon.VContainer.Utilities;
+using System.Collections.Generic;
+using VContainer;
+using System;
+
+namespace PuzzleDungeon.VContainer.GameStateMachine;
+
+public class PooledStateFactory : IStateFactory
+{
+    private readonly IObjectResolver _resolver;
+    
+    private readonly Dictionary<Type, IExitableState> _statesCache = new();
+
+    public PooledStateFactory(IObjectResolver resolver)
+    {
+        _resolver = resolver;
+    }
+    
+    public T GetState<T>() where T : class, IExitableState
+    {
+        if (_statesCache.TryGetValue(typeof(T), out IExitableState cachedState))
+        {
+            return (T)cachedState;
+        }
+        
+        T state = _resolver.CreateInstance<T>();
+        _statesCache[typeof(T)] = state;
+        return state;
+    }
+}
---
+++ src/PuzzleDungeon/Assets/_Project/Code/VContainer/Utilities/ObjectResolverExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using VContainer;
+
+namespace PuzzleDungeon.VContainer.Utilities;
+
+public static class ObjectResolverExtensions
+{
+    [Pure]
+    public static T CreateInstance<T>(this IObjectResolver resolver)
+    {
+        return (T)CreateInstance(resolver, typeof(T));
+    }
+    
+    [Pure]
+    public static object CreateInstance(this IObjectResolver resolver, Type type)
+    {
+        var constructors = type.GetConstructors();
+        
+        if (constructors.Length == 0)
+            throw new InvalidOperationException($"Type {type} has no public constructors");
+
+        var constructor = constructors[0];
+    
+        var parameters = constructor.GetParameters();
+        var args = new object[parameters.Length];
+        
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            args[i] = resolver.Resolve(parameters[i].ParameterType);
+        }
+    
+        return constructor.Invoke(args);
+    }
+}
