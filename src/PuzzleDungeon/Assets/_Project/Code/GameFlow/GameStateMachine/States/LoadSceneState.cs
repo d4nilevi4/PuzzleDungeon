45153aaa023875eb9ca4f461483a55bbbcc81ddc
d4nilevi4 <d4nia14@gmail.com>
2026-01-10 23:18:55 +0300

    remove unused logger dependency from LoadSceneState

--- src/PuzzleDungeon/Assets/_Project/Code/GameFlow/GameStateMachine/States/LoadSceneState.cs
+++ src/PuzzleDungeon/Assets/_Project/Code/GameFlow/GameStateMachine/States/LoadSceneState.cs
@@ -1,7 +1,6 @@
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using PuzzleDungeon.Core.GameStateMachine;
-using PuzzleDungeon.Core.Logger;
 using PuzzleDungeon.Core.SceneLoading;
 
 namespace PuzzleDungeon.GameFlow.GameStateMachine;
@@ -9,7 +8,6 @@ namespace PuzzleDungeon.GameFlow.GameStateMachine;
 public class LoadSceneState : IPayloadState<string>
 {
     private readonly ISceneLoader _sceneLoader;
-    private readonly ILogger _logger;
 
     public LoadSceneState(ISceneLoader sceneLoader)
     {
