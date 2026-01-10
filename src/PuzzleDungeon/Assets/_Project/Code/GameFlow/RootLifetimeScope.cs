c9f23d1588920f733ee10e6fcc7a4932875c17fc
d4nilevi4 <d4nia14@gmail.com>
2026-01-10 23:14:52 +0300

    add System factory registraintion in Root Lifetime Scope

--- src/PuzzleDungeon/Assets/_Project/Code/GameFlow/RootLifetimeScope.cs
+++ src/PuzzleDungeon/Assets/_Project/Code/GameFlow/RootLifetimeScope.cs
@@ -4,6 +4,7 @@ using PuzzleDungeon.Core.Logger;
 using PuzzleDungeon.Core.Progress;
 using PuzzleDungeon.Core.SceneLoading;
 using PuzzleDungeon.Core.Serializer;
+using PuzzleDungeon.Core.Systems;
 using PuzzleDungeon.GameFlow.GameStateMachine;
 using PuzzleDungeon.Gameplay.Progress;
 using PuzzleDungeon.Unity.Logger;
@@ -11,6 +12,7 @@ using PuzzleDungeon.Unity.Progress;
 using PuzzleDungeon.VContainer.GameStateMachine;
 using PuzzleDungeon.Unity.SceneLoading;
 using PuzzleDungeon.Unity.Serializer;
+using PuzzleDungeon.VContainer.Systems;
 using VContainer;
 using VContainer.Unity;
 
@@ -31,6 +33,13 @@ namespace PuzzleDungeon.GameFlow
             ConfigureProgress(builder);
             
             ConfigureSerialization(builder);
+
+            ConfigureSystemFactory(builder);
+        }
+
+        private void ConfigureSystemFactory(IContainerBuilder builder)
+        {
+            builder.Register<ISystemFactory, VContainerSystemFactory>(Lifetime.Singleton);
         }
 
         private void ConfigureSerialization(IContainerBuilder builder)
