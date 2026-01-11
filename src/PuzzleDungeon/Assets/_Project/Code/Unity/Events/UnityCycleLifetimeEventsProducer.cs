c8a7601b56ad376e6e6a387009908f181e40840b
d4nilevi4 <d4nia14@gmail.com>
2026-01-11 15:14:50 +0300

    add UnityCycleLifetimeEventProducer

---
+++ src/PuzzleDungeon/Assets/_Project/Code/Unity/Events/UnityCycleLifetimeEventsProducer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using PuzzleDungeon.Core.Events;
+using UnityEngine;
+
+namespace PuzzleDungeon.Unity.Events
+{
+    public class UnityCycleLifetimeEventsProducer : MonoBehaviour, ILifetimeEventsProducer
+    {
+        public event Action EventInitialize;
+        public event Action EventFixedUpdate;
+        public event Action EventUpdate;
+        public event Action EventLateUpdate;
+        public event Action EventDestroy;
+
+        private void Start()
+        {
+            EventInitialize?.Invoke();
+        }
+    
+        private void FixedUpdate()
+        {
+            EventFixedUpdate?.Invoke();
+        }
+    
+        private void Update()
+        {
+            EventUpdate?.Invoke();
+        }
+    
+        private void LateUpdate()
+        {
+            EventLateUpdate?.Invoke();
+        }
+    
+        private void OnDestroy()
+        {
+            EventDestroy?.Invoke();
+        }
+    }
+}
