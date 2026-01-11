25d56a698bbba973f1e5674240e9f5647f5319ea
d4nilevi4 <d4nia14@gmail.com>
2026-01-11 15:14:08 +0300

    add ILifetimeEventsProducer interface

---
+++ src/PuzzleDungeon/Assets/_Project/Code/Core/Events/ILifetimeEventsProducer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PuzzleDungeon.Core.Events;
+
+public interface ILifetimeEventsProducer
+{
+    event Action EventInitialize;
+    event Action EventFixedUpdate;
+    event Action EventUpdate;
+    event Action EventLateUpdate;
+    event Action EventDestroy;
+}
