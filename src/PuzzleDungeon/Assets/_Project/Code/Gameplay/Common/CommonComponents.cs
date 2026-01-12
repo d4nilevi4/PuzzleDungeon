d2161b2b9722310fa5dd0e95022541f2caaec6ba
d4nilevi4 <d4nia14@gmail.com>
2026-01-12 15:10:36 +0300

    move transfom components in transform feature

--- src/PuzzleDungeon/Assets/_Project/Code/Gameplay/Common/CommonComponents.cs
+++ src/PuzzleDungeon/Assets/_Project/Code/Gameplay/Common/CommonComponents.cs
@@ -4,7 +4,3 @@ using PuzzleDungeon.Core.View;
 namespace PuzzleDungeon.Gameplay;
 
 [Game] public struct EntityView : IComponent { public IEntityView Value; }
-
-[Game] public struct Position : IComponent { public Vector3 Value; }
-[Game] public struct Rotation : IComponent { public Quaternion Value; }
-[Game] public struct Scale : IComponent { public Vector3 Value; }
