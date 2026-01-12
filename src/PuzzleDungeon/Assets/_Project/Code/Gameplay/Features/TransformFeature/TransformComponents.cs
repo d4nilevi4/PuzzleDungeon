ca7cc77dbfa70f8c702a6d4e9901a92e9d6e844b
d4nilevi4 <d4nia14@gmail.com>
2026-01-12 13:08:52 +0300

    add transform feature

---
+++ src/PuzzleDungeon/Assets/_Project/Code/Gameplay/Features/TransformFeature/TransformComponents.cs
@@ -0,0 +1,7 @@
+﻿using Leontitas;
+
+namespace PuzzleDungeon.Gameplay.Transform;
+
+[Game] public struct Position : IComponent { public Vector3 Value; }
+[Game] public struct Rotation : IComponent { public Quaternion Value; }
+[Game] public struct Scale : IComponent { public Vector3 Value; }
