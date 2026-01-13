40b91fed56bb32ae175060a85c594d50532333d3
d4nilevi4 <d4nia14@gmail.com>
2026-01-13 10:37:44 +0300

    add EntityView Registrar

---
+++ src/PuzzleDungeon/Assets/_Project/Code/Unity/Registrars/ViewRegistrar.cs
@@ -0,0 +1,16 @@
+﻿using Leontitas;
+using PuzzleDungeon.Core;
+using PuzzleDungeon.Unity.View;
+
+namespace PuzzleDungeon.Gameplay
+{
+    public sealed class ViewRegistrar : GameEntityComponentRegistrar
+    {
+        public UnityEntityView View;
+    
+        public override void RegisterComponents(GameEntity entity)
+        {
+            entity.AddEntityView(View);
+        }
+    }
+}
